d13a54e64acfd1d8c26dcf592200ae43499d545b
Leonardo Benedeti <leonardobenedeti@gmail.com>
2020-09-14 00:47:24 -0300

    Ajuste de layout da pagina de usuarios

--- lib/features/user/user.widget.dart
+++ lib/features/user/user.widget.dart
@@ -1,4 +1,5 @@
 import 'package:Pruuu/features/auth/bloc/auth_bloc.dart';
+import 'package:bubble/bubble.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,6 +11,8 @@ class UserWidget extends StatefulWidget {
 
 class _UserWidgetState extends State<UserWidget> {
   FirebaseUser user;
+  String textoDisclaimer =
+      "App criado como um desafio para uma oportunidade de trabalho onde precisava criar um app com algumas funcionalidades parecidas com a do Twitter.";
 
   @override
   void initState() {
@@ -68,76 +71,146 @@ class _UserWidgetState extends State<UserWidget> {
 
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Column(
               children: [
-                Text(
-                  "Usuário conectado",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                FlatButton(
-                  child: Text(
-                    "Cancel",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  onPressed: () => Navigator.pop(context),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            user != null && user.photoUrl != null
-                ? ClipOval(
-                    child: Image.network(user.photoUrl),
-                  )
-                : ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(
-                      child: Icon(
-                        Icons.person_outline,
-                        color: Colors.white,
-                      ),
-                      color: Colors.black,
-                    ),
-                  ),
-            SizedBox(
-              width: 16,
-            ),
-            Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                      user != null && user.displayName != null
-                          ? name
-                          : "glygjygj",
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Usuário conectado",
                       style: TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
-                      )),
-                  Text("@$username"),
+                      ),
+                    ),
+                    FlatButton(
+                      child: Text(
+                        "Fechar",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 160,
+                  width: 160,
+                  child: user != null && user.photoUrl != null
+                      ? ClipOval(
+                          child: Image.network(user.photoUrl),
+                        )
+                      : ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            child: Icon(
+                              Icons.person_outline,
+                              color: Colors.white,
+                            ),
+                            color: Colors.black,
+                          ),
+                        ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "@$username",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 60,
+                    width: 60,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.asset("assets/images/perfil-leo.png"),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 100,
+                    child: Bubble(
+                      margin: BubbleEdges.only(top: 10),
+                      alignment: Alignment.topLeft,
+                      nip: BubbleNip.leftTop,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Disclaimer",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "App criado como um desafio para uma oportunidade de trabalho onde precisava criar um app com algumas funcionalidades parecidas com a do Twitter.",
+                            maxLines: 10,
+                            overflow: TextOverflow.ellipsis,
+                            softWrap: false,
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Todo código do app permanecerá aberto e livre para todos.",
+                            maxLines: 10,
+                            overflow: TextOverflow.ellipsis,
+                            softWrap: false,
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          FlatButton(
+                            child: Text("Acessar repo"),
+                            color: Colors.black,
+                            onPressed: () {},
+                            textColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8)),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
-            ),
-            SizedBox(
-              height: 16,
             ),
             FlatButton(
               child: Text(
                 "Sair do app",
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),
               ),
               onPressed: () =>
                   BlocProvider.of<AuthBloc>(context).add(SignOutApp(context)),
-              textColor: Colors.red,
+              textColor: Colors.redAccent[700],
             )
           ],
         ),
