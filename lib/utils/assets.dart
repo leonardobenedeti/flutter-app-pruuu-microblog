1c67c7e3b1d797662aef52c042a66c51c841cff1
Leonardo Benedeti <leonardo.macedo@dextra-sw.com>
2021-02-01 20:58:17 -0300

    Ajuste no path de dois assets

--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -2,6 +2,6 @@ class Assets {
   static const bannerAuth = "assets/images/bannerAuth";
   static const defaultExtension = ".png";
   static const myProfile = "assets/images/perfil-leo.png";
-  static const errorState = "error_state.png";
-  static const emptyState = "empty_state.png";
+  static const errorState = "assets/images/error_state.png";
+  static const emptyState = "assets/images/empty_state.png";
 }
